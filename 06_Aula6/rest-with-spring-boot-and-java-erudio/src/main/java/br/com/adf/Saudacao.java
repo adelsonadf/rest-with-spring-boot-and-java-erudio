14a699410cec810a2f90e54f90f06b1e7637a4a0
adelsonadf <adelsonadf@terra.com.br>
2024-07-06 21:14:11 -0300

    aula6

---
+++ 06_Aula6/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/adf/Saudacao.java
@@ -0,0 +1,19 @@
+package br.com.adf;
+
+public class Saudacao {
+		private final long id;
+		private final String content;
+		public Saudacao(long id, String content) {
+			this.id = id;
+			this.content = content;
+		}
+		public long getId() {
+			return id;
+		}
+		public String getContent() {
+			return content;
+		}
+		
+	
+
+}
